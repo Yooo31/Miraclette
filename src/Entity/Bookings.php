c0fbea58d28ff2f179bacefb84cf0049422d0d24
Yoan <mrts.yoan@gmail.com>
2024-07-03 17:04:27 +0200

    Booking table

---
+++ src/Entity/Bookings.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookingsRepository::class)]
+class Bookings
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $capacity = null;
+
+    #[ORM\Column]
+    private ?int $location = null;
+
+    #[ORM\Column]
+    private ?bool $status = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $nb = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): static
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getLocation(): ?int
+    {
+        return $this->location;
+    }
+
+    public function setLocation(int $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getNb(): ?string
+    {
+        return $this->nb;
+    }
+
+    public function setNb(?string $nb): static
+    {
+        $this->nb = $nb;
+
+        return $this;
+    }
+}
