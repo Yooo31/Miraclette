0605f51b569f988af7a115ee116846f7c847b993
Yoan <mrts.yoan@gmail.com>
2024-07-03 18:15:57 +0200

    App login and display home only where login is done

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/', name: 'home.')]
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        // Get current username and ROLE
+        $user = $this->getUser();
+        $username = $user->getUsername();
+        $role = $user->getRoles()[0]; // Assuming only one role per user
+
+        // Render the home page with user data
+        return $this->render('home/index.html.twig', [
+            'username' => $username,
+            'role' => $role,
+        ]);
+    }
+}
